4c23401b90b1ce115e08574b6f4da912b2487c69
Roman Avdeienko <avroman91@gmail.com>
2021-09-14 18:28:10 +0300

    tests deleted.

--- src/main/java/org/example/phoneBook/Main.java
+++ src/main/java/org/example/phoneBook/Main.java
@@ -11,26 +11,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
-//        ContactList test = new ContactList();
-//        test.add(new Contact("Vasia", "031"));
-//        try(ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("Contacts.obj"))) {
-//            objectOutputStream.writeObject(test);
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("Contacts.obj"))) {
-//            ContactList test2 = (ContactList) objectInputStream.readObject();
-//            System.out.println(test2);
-//        } catch (FileNotFoundException e) {
-//            throw  new RuntimeException("File Not found");
-//        } catch (IOException | ClassNotFoundException e) {
-//            e.printStackTrace();
-//        } ;
-
+        
         List<MenuAction> actions = new ArrayList<>();
         Menu menu = new Menu(actions);
         ContactsService memory = menu.chooseType();
